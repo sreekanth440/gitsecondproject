554c0f1f33d09f46564be0be98f8aa48dcc8d004
sreekanth440 <sreekanthroyals.sr@gmail.com>
2019-09-26 20:46:14 +0530

    my name sreekanth

---
+++ src/org/computer/Computer.java
@@ -0,0 +1,9 @@
+package org.computer;
+
+public class Computer {
+	public void computerModel() {
+		System.out.println("computer model is dellxps");
+
+	}
+
+}
---
+++ src/org/computer/Desktop.java
@@ -0,0 +1,14 @@
+package org.computer;
+
+public class Desktop extends Computer {
+	public void desktopSize() {
+		System.out.println("desktop size is 123cm");
+
+	}
+public static void main(String[] args) {
+	Desktop s=new Desktop();
+	s.desktopSize();
+	s.computerModel();
+}
+}
+ 
